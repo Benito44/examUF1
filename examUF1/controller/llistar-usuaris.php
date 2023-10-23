2c899122df3d88ce5f6ebc2dc6fc9edcdef4fe45
BenitoMF <b.martinez2@sapalomera.cat>
2023-10-23 19:08:01 +0200

    Ex5 Mostra els usuaris si eta administrador

---
+++ examUF1/controller/llistar-usuaris.php
@@ -0,0 +1,30 @@
+<?php
+// ex5 
+require 'session.php';
+require '../model/db-connection.php';
+function mostrar(){
+
+
+  try {
+    session_start();
+    $id = getSessionUserId();
+    $connexio = getConnection();
+    // Fem la secuencia per insertar els usuaris
+    $statement = $connexio->prepare("SELECT * FROM users WHERE id != ?");
+    $statement->bindParam(1,$id);
+    $statement->execute();
+    echo '<section><ul>';
+while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+    echo '<li>' . $row["id"] . " - " . $row["nickname"] . '</li>';
+}
+echo '</ul></section>';
+
+      } 
+    catch (Exception $e) {
+        echo "Error:" .  $e->getMessage();
+    }
+}
+
+
+include '../view/llistar-view.php';
+?>
---
+++ examUF1/view/llistar-view.php
@@ -0,0 +1,18 @@
+<!-- ex5 -->
+<!DOCTYPE html>
+<!-- Benito Martinez Florido -->
+<html lang="en">
+<head>
+<link rel="stylesheet" type="text/css" href="../Estils/estils.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+        </form>
+        <!-- Funció per mostrar els usuaris -->
+        <span class="mostrar"><?php mostrar(); ?></span>
+        <a href="index.php">Torna al index</a>
+    </body>
+</html>
+
